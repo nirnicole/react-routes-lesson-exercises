4ecb5e3f26878b7145892e764c2e6aef950e5bc1
Nir Nicole <Niko426@gmail.com>
2022-11-09 16:45:39 +0200

    add fentity and fentities support  routs

--- src/components/Fentities.js
+++ src/components/Fentities.js
@@ -1,24 +1,35 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import '../styles/fentity-directory.css'
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import "../styles/fentity-directory.css"
 
 class Fentities extends Component {
-    render() {
-        return (
-            <div>
-                <h1 id="fentities-title">{/*Get from `match`*/}</h1>
-                <div id="fentities-container">
-                    {fentities.map(f => {
-                        return (
-                            <div className="mini">
-                                <img className="directory-img" src={f.imgUrl} alt="" />
-                                <span>{f.name}</span>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>)
-    }
+	render() {
+		let fentitiesCategory = this.props.match.params.fentities
+		const fentities = this.props.state[fentitiesCategory]
+		return (
+			<div>
+				<h1 id="fentities-title">{fentitiesCategory}</h1>
+				<div id="fentities-container">
+					{fentities.map((f) => {
+						return (
+							<Link
+								to={`/directory/${fentitiesCategory}/${f.name}`}
+							>
+								<div className="mini">
+									<img
+										className="directory-img"
+										src={f.imgUrl}
+										alt=""
+									/>
+									<span>{f.name}</span>
+								</div>
+							</Link>
+						)
+					})}
+				</div>
+			</div>
+		)
+	}
 }
 
 export default Fentities
--- src/components/Fentity.js
+++ src/components/Fentity.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import '../styles/fentity.css'
-
+import React, { Component } from "react"
+import "../styles/fentity.css"
 
 class Fentity extends Component {
-    render() {
-        return (
-            <div id="creature-container">
-                <h1>{fentity.name}</h1>
-                <img src={fentity.imgUrl} alt=""/>
-                <div className="title">Power:</div>
-                <div className="power text"> {fentity.power}</div>
-                <div className="other text">{fentity.other}</div>
-            </div>
-        )
-    }
+	render() {
+		let fentitiesCategory = this.props.match.params.fentities
+		let fentitiesSubject = this.props.match.params.name
+		const fentity = this.props.state[fentitiesCategory].find(
+			(x) => x.name === fentitiesSubject
+		)
+		return (
+			<div id="creature-container">
+				<h1>{fentity.name}</h1>
+				<img src={fentity.imgUrl} alt="" />
+				<div className="title">Power:</div>
+				<div className="power text"> {fentity.power}</div>
+				<div className="other text">{fentity.other}</div>
+			</div>
+		)
+	}
 }
 
-export default Fentity;
+export default Fentity
